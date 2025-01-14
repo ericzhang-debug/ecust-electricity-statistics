b4038338c8f5e7533501046da8495310a4559ceb
Zhehao Zhang <15364519511@163.com>
2025-01-14 09:30:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ if not(PUSH_PLUS_TOKEN == "" or PUSH_PLUS_TOKEN==None or PUSH_PLUS_TOKEN=="error
     for item in reversed(last_two_items):
         tablestr+=f'{index}\t{item["time"]}\t{item["kWh"]}kWh\n'
         index+=1
-    text=f'当前剩余电量：{remain}kWh\n个人信息：{buildid[0]}号楼{roomid[0]}室\n\n统计时间：{stime}\n\n----------------------- 最近10天数据:\n'
+    text=f'当前剩余电量：{remain}kWh\n个人信息：{buildid[0]}号楼{roomid[0]}室\n\n统计时间：{stime}\n\n-----------------------\n 最近10天数据:\n'
     text+=(tablestr+"\n")
     print(text)
     print(PUSH_PLUS_TOKEN)
